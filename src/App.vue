9a346e053d3edc09f3bdfe3dc9ae048c51ab0cea
fernando david martinez martelo <famartinez.ti@gmail.com>
2022-11-30 23:11:04 +0100

    listo

--- src/App.vue
+++ src/App.vue
@@ -57,14 +57,13 @@ const getCategoryPath = (categories, categoryName) => {
 };
 
 // OUTPUT SAMPLES
-// getCategoryPath(categories, 'category4')
 console.log(getCategoryPath(categories, 'category4')); // should output: '/category1/category3/category4'
 console.log(getCategoryPath(categories, 'category2')); // should output: '/category1/category2'
 console.log(getCategoryPath(categories, 'category5')); // should output: '/category5'
 
 </script>
 <template>
-  <h1>hola</h1>
+  <h1>hola, por favor abra la consola</h1>
 </template>
 
 <style scoped>
